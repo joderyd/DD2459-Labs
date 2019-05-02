9bbca242a12b65dfc60b5186ff9476f0c1b95ab7
Jonathan Öderyd <47157963+joderyd@users.noreply.github.com>
2019-05-02 21:20:48 +0200

    Update ListOperators.java

--- Lab2/src/ListOperators.java
+++ Lab2/src/ListOperators.java
@@ -16,37 +16,36 @@ public class ListOperators {
 
 
     /**
-     * @requires A is sorted
-     * @ensures A is returned in sorted order
-     * @exception NullPointerException if A == null or key == null
-     * @exception IllegalArgumentException if A is empty
      *
      * @param A array of integer values of any length. must be sorted
      * @param key integer value that is searched for in A. must not be null
      * @return the index of the key value in the array. If key does not exist in A, -1 is returned
+     *
+     * @exception NullPointerException if A == null
+     * @exception IllegalArgumentException if A is empty
      */
     public static int binarySearch(int[] A, int key) throws NullPointerException, IllegalArgumentException{
-        if(A==null) {
+        //@requires A is sorted
+        //@ensures index of key is returned if key exists in A. otherwise -1 is returned.
+        if (A == null) {
             throw new NullPointerException("Array or key must not be null!");
         }
-
-        int x = 0;
-        int l = 1;
-        int r = A.length -1;
-        if(r<0){
+        if (A.length < 1) {
             throw new IllegalArgumentException("List must not be empty");
         }
-
-        do{
-            x =(l+r)/2;
-            if(key<A[x]){
-                r =x-1;
+        
+        int x = 0;
+        int l = 1;
+        int r = A.length - 1;
+        while( key != A[x] && (l<=r)){
+            x = (l + r) / 2;
+            if (key < A[x]) {
+                r = x - 1;
+            } else {
+                l = x + 1;
             }
-            else{
-                l =x+1;
-            }
-        }while( key != A[x] && (l<=r));
-
+        }
+        
         if(key==A[x]){
             return x;
         }
@@ -64,8 +63,11 @@ public class ListOperators {
      *
      * @param A array of integer values of any length. must not be null
      * @return array of same length and the same values as A, but in sorted order
+     * @exception NullPointerException if A == null
      */
     public static int[] bubbleSort(int[] A){
+        //@requires A.length >0
+        //@ensures A is returned in sorted order
         if(A==null){
             throw new NullPointerException("Array must not be null");
         }
@@ -92,11 +94,10 @@ public class ListOperators {
      * @param A array of integer values of any length.
      * @param key value that will be be queried for membership
      * @return true if key exists in A, otherwise false
-     *
-     * @requires A!=null && A.length >=1
-     * @ensures that functions returns true if key exists in A, otherwise false
      */
     public static synchronized boolean membership(int[] A, int key) {
+        //@requires A!=null && A.length >=1 && No element in A == null
+        //@ensures that functions returns true if key exists in A, otherwise false
         int[] sorted = bubbleSort(A);
         int n = binarySearch(sorted, key);
         return(n!= -1);
