d27ede77e1659d2d6e77ef04311aebb711141e67
Jonathan Öderyd <47157963+joderyd@users.noreply.github.com>
2019-05-04 16:33:49 +0200

    Update ListOperators.java

--- Lab2/src/ListOperators.java
+++ Lab2/src/ListOperators.java
@@ -10,53 +10,6 @@
 
 public class ListOperators {
     private ListOperators(){}
-    
-    
-    
-    /**
-     * Method searches for a key value in the input Array and returns an integer value that should be
-     * n if the key occurs in the input array, in position n and -1 if it does not occur anywhere in 
-     * the array
-     *
-     * @param A array of integer values of any length. must be sorted
-     * @param key integer value that is searched for in A. must not be null
-     * @return the index of the key value in the array. If key does not exist in A, -1 is returned
-     *
-     * @exception NullPointerException if A == null
-     * @exception IllegalArgumentException if A is empty
-     */
-    public static int binarySearch(int[] A, int key) throws NullPointerException, IllegalArgumentException{
-        //@requires A is sorted
-        if (A == null) {
-            throw new NullPointerException("Array or key must not be null!");
-        }
-        if (A.length < 1) {
-            throw new IllegalArgumentException("List must not be empty");
-        }
-
-        int x = 0;
-        int l = 1;
-        int r = A.length - 1;
-        while( key != A[x] && (l<=r)){
-            x = (l + r) / 2;
-            if (key < A[x]) {
-                r = x - 1;
-            } else {
-                l = x + 1;
-            }
-        }
-
-        //@ensures index of key is returned if key exists in A. otherwise -1 is returned
-        if(key==A[x]){
-            return x;
-        }
-        else{
-            return -1;
-        }
-    }
-
-
-
 
 
     /**
@@ -69,20 +22,68 @@ public class ListOperators {
     public static int[] bubbleSort(int[] A){
         if(A==null){
             throw new NullPointerException("Array must not be null");
+            // Mutation #1: change the String "Array must not be null" to something else
         }
-        //@requires A.length >0
+        //@requires A to be a list of integers of any length
         for(int i=0; i<A.length;i++) {
             for(int j=1; j< (A.length-i); j++) {
                 if(A[j-1] > A[j]) {
-                    int swap = A[j-1];
+                    int swap = A[j-1];      // Mutation #2:  change to <int swap = A[j]>
                     A[j-1] = A[j];
-                    A[j] = swap;
+                    A[j] = swap;            // Mutation #3:  Comment this line
                 }
             }
         }
         //@ensures A is returned in sorted order
         return A;
     }
+
+
+
+
+    /**
+     * Method searches for a key value in the input Array and returns an integer value that should be
+     * n if the key occurs in the input array, in position n and -1 if it does not occur anywhere in
+     * the array
+     *
+     * @param A array of integer values of any length. must be sorted
+     * @param key integer value that is searched for in A. must not be null
+     * @return the index of the key value in the array. If key does not exist in A, -1 is returned
+     *
+     * @exception NullPointerException if A == null
+     * @exception IllegalArgumentException if A is empty
+     */
+    public static int binarySearch(int[] A, int key) throws NullPointerException, IllegalArgumentException{
+        if (A == null) {
+            throw new NullPointerException("Array must not be null");
+        }
+        if (A.length < 1) {
+            throw new IllegalArgumentException("List must not be empty");   // Mutation #4:  Comment this line
+        }
+
+        //@requires A is sorted
+        int x = 0;
+        int l = 1;
+        int r = A.length -1;
+        while( key != A[x] && (l<=r)){      // Mutation #5:  Change "&&" to "||"
+        //while( key != A[x] || (l<=r)){    // The test cases will not terminate because of infinite loop
+            x = (l + r) / 2;
+            if (key < A[x]) {
+                r= x-1;          // Mutation #6: Change to "r= x+1"
+            } else {
+                l= x+1;
+            }
+        }
+        //@ensures index of key is returned if key exists in A. otherwise -1 is returned
+        if(key==A[x]){
+            return x;
+        }
+        else{
+            return -1;
+        }
+    }
+
+
 
 
 
