b9b66272226b78bf539431e891e87d63e740ab0c
Jonathan Öderyd <47157963+joderyd@users.noreply.github.com>
2019-05-04 14:28:03 +0200

    Update RandomTestList.java

--- Lab2/src/RandomTestList.java
+++ Lab2/src/RandomTestList.java
@@ -3,73 +3,72 @@ import org.junit.jupiter.api.Test;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class RandomTestList {
-    private static final String TESTFILES_PATH = "C:\\Users\\jonth\\git\\solRel\\Lab2\\src\\testFiles";
-    private static final String FILENAME_RANDOM = "randomTests.txt";
-    private static final int numOfArrays = 30;
-    private static final int maxInt = 30;
-    //private static int[] testArray;
-    private static ArrayList<int[]> trueArrays, falseArrays;
-    private static StringBuilder stringBuilder;
-    private static int trueKeysFound, trueKeysNotFound;
-    private static int falseKeysNotFound, falseKeysFound;
-
-
-    static void createTestArrays(){
-        TestUtils.createRandomTestArrays(FILENAME_RANDOM, numOfArrays, maxInt, 20, 5);
-    }
+    static final String FILENAME_RANDOM = "src/testFiles/randomizedTests.txt";
+    static int numOfArrays = 100;
+    static int maxInt = 300;
+    static ArrayList<int[]> testArrays;
+    static StringBuilder stringBuilder;
+    static int trueKeysFound, trueKeysNotFound;
+    static int falseKeysNotFound, falseKeysFound;
 
 
     @BeforeAll
-    static void init(){
+    public static void init(){
         trueKeysFound = 0;
         trueKeysNotFound = 0;
         falseKeysFound = 0;
         falseKeysNotFound = 0;
+        testArrays = new ArrayList<>();
         stringBuilder = new StringBuilder();
-        trueArrays = new ArrayList<>();
-        falseArrays = new ArrayList<>();
-        createTestArrays();
-        //TestUtils.createPairwiseTestArrays("pairwiseTests.txt", 20, 1);
-        File fileRandom = new File(TESTFILES_PATH + File.separator + FILENAME_RANDOM);
-        try {
-            BufferedReader randomBufferedReader = new BufferedReader(new FileReader(fileRandom));
-            String nextRandomLine = randomBufferedReader.readLine();
-            while(nextRandomLine!=null){
-                int[] a = TestUtils.stringToArray(nextRandomLine);
-                trueArrays.add(a);
-                falseArrays.add(a);
-                nextRandomLine = randomBufferedReader.readLine();
-            }
-            randomBufferedReader.close();
-        } catch (Exception e) { e.printStackTrace(); }
+        File file = new File(FILENAME_RANDOM);
+        if(file.exists()){
+            file.delete();
+        }
+        TestUtils.createRandomizedTestArrays(FILENAME_RANDOM, numOfArrays, 20, maxInt, 5);
+        readTests(FILENAME_RANDOM, 200);
     }
 
 
 
+    static synchronized void readTests(String filename, int maxArrays){
+        File file = new File(filename);
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String nextRandomLine = bufferedReader.readLine();
+            int counter = 0;
+            while(nextRandomLine!=null && counter < maxArrays){
+                counter++;
+                int[] a = TestUtils.stringToArray(nextRandomLine);
+                testArrays.add(a);
+                nextRandomLine = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+        } catch (Exception e) { e.printStackTrace(); }
 
-    @BeforeEach
-    synchronized void setUp(){ }
+    }
 
 
-    @RepeatedTest(20)
+    @RepeatedTest(50)
     public synchronized void falseMembershipTest(RepetitionInfo repetitionInfo) {
-        System.out.println("\n\nTest repetition #" + repetitionInfo.getCurrentRepetition());
-        if (falseArrays.size() > 0) {
-            int[] A = falseArrays.remove(0);
+        if (testArrays.size() > 0) {
+            int[] A = testArrays.remove(0);
+            int falseKey = maxInt+1;
             boolean F = ListOperators.membership(A, maxInt+1);
-            System.out.println("key= " + maxInt+1 + "\nArray= " + TestUtils.arrayToString(A));
             assertFalse(F);
+
+            // Logging test result
+            stringBuilder.append("\n\nTest repetition #" + repetitionInfo.getCurrentRepetition() +
+                    "\nkey= " + maxInt+1 + "\nArray= " + TestUtils.arrayToString(A));
             if(!F){
-                stringBuilder.append("key = " + maxInt+1 + " was successfully not found!\n");
+                stringBuilder.append("key = " + falseKey + " was successfully not found!\n");
                 falseKeysNotFound ++;
             }
             else{
-                stringBuilder.append("key = " + maxInt+1 + " was unsuccessfully found!\n");
+                stringBuilder.append("key = " + falseKey + " was unsuccessfully found!\n");
                 falseKeysFound ++;
             }
             stringBuilder.append("=====================\n");
@@ -80,22 +79,18 @@ public class RandomTestList {
 
 
 
-    @RepeatedTest(20)
-    public void trueMembershipTest(RepetitionInfo repetitionInfo){
-        System.out.println("\n\nTest repetition #" + repetitionInfo.getCurrentRepetition());
-        if(trueArrays.size()>0) {
-            int[] A = trueArrays.remove(0);
-            Random random = new Random();
-            int index = random.nextInt(A.length);
-            int key = A[index];
-            System.out.println("key= " + key + "\nArray= " + TestUtils.arrayToString(A)
-            + "\nSorted= " + TestUtils.arrayToString(ListOperators.bubbleSort(A)));
-
+    @RepeatedTest(50)
+    public synchronized void trueMembershipTest(RepetitionInfo repetitionInfo){
+        if(testArrays.size()>0) {
+            int[] A = testArrays.remove(0);
+            int idx = new Random().nextInt(A.length);
+            int key = A[idx];
             boolean T = ListOperators.membership(A, key);
             assertTrue(T);
 
-            stringBuilder.append("Test #" + repetitionInfo.getCurrentRepetition() + "\n");
-            stringBuilder.append("testList = " + TestUtils.arrayToString(A) + "\n");
+            // Logging test result
+            stringBuilder.append("\n\nTest repetition #" + repetitionInfo.getCurrentRepetition() +
+                    "\nkey= " + key + "\nArray= " + TestUtils.arrayToString(A));
             if(T){
                 stringBuilder.append("key = " + key + " was successfully found!\n");
                 trueKeysFound ++;
@@ -104,6 +99,7 @@ public class RandomTestList {
                 stringBuilder.append("key = " + key + " was NOT found!\n");
                 trueKeysNotFound ++;
             }
+            stringBuilder.append("========================\n");
         }
     }
 
@@ -138,15 +134,23 @@ public class RandomTestList {
 
     @AfterAll
     static void printResult(){
+        File file = new File("src/testFiles/randomizedTest_RESULT.txt");
+        if(file.exists()){
+            file.delete();
+        }
         try {
-            FileWriter fileWriter = new FileWriter(new File("src/testFiles/randomTestResult"), true);
-            fileWriter.write("~TEST RESULT~" +
-                    "\ntrueKeysFound = " + trueKeysFound +
-                    "\ntrueKeysNotFound = " + trueKeysNotFound +
-                    "\nfalseKeysFound = " + falseKeysFound +
-                    "\nfalseKeysNotFound = " + falseKeysNotFound +
-                    "\n\n\n\n Detailed test result:\n");
-            fileWriter.write(stringBuilder.toString());
+            FileWriter fileWriter = new FileWriter(new File("src/testFiles/randomizedTest_RESULT.txt"), true);
+            String resultSummary = "\n\n~ RANDOMIZED TEST RESULT~" +
+                    "\n--- summary ---" +
+                    "\ntrueKeysFound= " + trueKeysFound +
+                    "\ntrueKeysNotFound= " + trueKeysNotFound +
+                    "\nfalseKeysFound= " + falseKeysFound +
+                    "\nfalseKeysNotFound= " + falseKeysNotFound +
+                    "\n-----------------------------------"+
+                    "\n\nDETAILED TEST RESULT:\n----------------------------------\n";
+
+            fileWriter.write(resultSummary + stringBuilder.toString());
+            fileWriter.flush();
             fileWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
